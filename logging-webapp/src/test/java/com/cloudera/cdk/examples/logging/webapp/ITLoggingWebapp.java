89f3f3f9b1245e1a57f4f9478fa1fe133b24ca5c
Tom White <tom@cloudera.com>
2013-09-04 14:00:53 +0100

    ITLoggingWebapp now passes, although we have to add a copy of log4j.properties since it isn't being picked up from the webapp correctly.

--- logging-webapp/src/test/java/com/cloudera/cdk/examples/logging/webapp/ITLoggingWebapp.java
+++ logging-webapp/src/test/java/com/cloudera/cdk/examples/logging/webapp/ITLoggingWebapp.java
@@ -15,23 +15,31 @@
  */
 package com.cloudera.cdk.examples.logging.webapp;
 
+import com.cloudera.cdk.examples.logging.CreateDataset;
 import com.cloudera.cdk.examples.logging.DropDataset;
+import com.cloudera.cdk.examples.logging.ReadDataset;
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.io.PrintStream;
-import org.apache.catalina.LifecycleException;
 import org.apache.catalina.core.AprLifecycleListener;
 import org.apache.catalina.core.StandardServer;
 import org.apache.catalina.startup.Tomcat;
 import org.apache.hadoop.util.Tool;
+import org.apache.http.client.fluent.Request;
 import org.hamcrest.Matcher;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.any;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.assertThat;
+import static org.junit.matchers.JUnitMatchers.containsString;
 
 public class ITLoggingWebapp {
+
+  private Tomcat tomcat;
+
   @Before
   public void setUp() throws Exception {
     // drop dataset in case it already exists
@@ -42,12 +50,23 @@ public class ITLoggingWebapp {
 
   @Test
   public void test() throws Exception {
+    run(new CreateDataset());
+    for (int i = 1; i <= 10; i++) {
+      get("http://localhost:8080/logging-webapp/send?message=Hello-" + i);
+    }
+    Thread.sleep(30000); // wait for events to be flushed to HDFS
+    run(containsString("{\"id\": 10, \"message\": \"Hello-10\"}"), new ReadDataset());
+    run(new DropDataset());
+  }
 
+  @After
+  public void tearDown() throws Exception {
+    stopTomcat();
   }
 
   private void startTomcat() throws Exception {
     String appBase = "target/wars/logging-webapp.war";
-    Tomcat tomcat = new Tomcat();
+    tomcat = new Tomcat();
     tomcat.setPort(8080);
 
     tomcat.setBaseDir(".");
@@ -55,14 +74,22 @@ public class ITLoggingWebapp {
 
     String contextPath = "/logging-webapp";
 
-    // Add AprLifecycleListener
-    StandardServer server = (StandardServer)tomcat.getServer();
-    AprLifecycleListener listener = new AprLifecycleListener();
-    server.addLifecycleListener(listener);
+    StandardServer server = (StandardServer) tomcat.getServer();
+    server.addLifecycleListener(new AprLifecycleListener());
 
     tomcat.addWebapp(contextPath, appBase);
     tomcat.start();
-    tomcat.getServer().await();
+  }
+
+  private void stopTomcat() throws Exception {
+    if (tomcat != null) {
+      tomcat.stop();
+    }
+  }
+
+  private void get(String url) throws IOException {
+    assertThat(Request.Get(url).execute().returnResponse().getStatusLine()
+        .getStatusCode(), equalTo(200));
   }
 
   private static void run(Tool tool, String... args) throws Exception {
